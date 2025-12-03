befd5be41acf1dffdc77547d8e8e681f34301954
RHYTHAN VARSHA JANAKIRAM <152013507+Rhythanvarsha@users.noreply.github.com>
2025-12-02 20:38:01 -0800

    Update ground.java

--- read/ground.java
+++ read/ground.java
@@ -1,23 +1,23 @@
 public class grouund {
     public static void main(String[] args) {
         System.out.println("Welcome to Ground Booking System");
-        
-    String[] grounds={"Main Ground","Practice Field","Indoor Court"};
+
+        String[] grounds={"Foot Ball Ground","Volley Ball Ground","Cricket Ground"};
         String[] bookings =new String[10];
         int count =0;
-        
-    bookings[count++] =grounds[0]+" 2025-12-03,Hari";
+
+        bookings[count++] =grounds[0]+" 2025-12-03,Hari";
         bookings[count++]=grounds[1]+" 2025-12-03, Rhythan";
         bookings[count++]=grounds[2]+" 2025-12-04,Varsha";
-        
-        
-      System.out.println("\nCurrent Bookings:");
-       
+
+
+        System.out.println("\nCurrent Bookings:");
+
         for(int  i=0;i<count;i++){
             System.out.println((i+1)+ "."+bookings[i]);
         }
-        
-      System.out.println("\nTotal Bookings:"+count);
+
+        System.out.println("\nTotal Bookings:"+count);
         System.out.println("Available Slots:"+(10-count));
     }
 }
